2f49ecaf52d49deea1216174c48642e5057102b5
keytash1 <max.ryabets@mail.ru>
2025-11-12 14:55:50 +0300

    l1_3

---
+++ l1/l1_3/l1_3.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"sync"
+	"time"
+)
+
+func worker(id int, jobs <-chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for num := range jobs {
+		fmt.Println(id, " ", num)
+	}
+}
+
+func main() {
+	number := flag.Int("workers", 3, "number of workers")
+	flag.Parse()
+	var wg sync.WaitGroup
+	jobs := make(chan int, 10)
+	for i := 0; i < *number; i++ {
+		wg.Add(1)
+		go worker(i, jobs, &wg)
+	}
+	counter := 0
+	for {
+		jobs <- counter
+		counter++
+		time.Sleep(100 * time.Millisecond)
+		if counter == 100 {
+			break
+		}
+	}
+	close(jobs)
+	wg.Wait()
+}
